c5ec0fecd8d217f78444dae263dfd2a6cc3cea9a
satyasandhya97 <satyasandhyabiswal97@gmail.com>
2026-06-20 23:08:54 +0530

    Setup the github APP and Add allowed development origins to Next.js configuration.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['peculiar-muck-contour.ngrok-free.dev'],
 };
 
 export default nextConfig;
